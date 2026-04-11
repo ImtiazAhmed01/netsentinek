324e7d75d19bc6cfdcd560c48c834089a2d4fb76
ImtiazAhmed01 <imtiazahm21@gmail.com>
2026-04-12 03:29:57 +0600

    client side setup done

---
+++ netsentinel/client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-900">
+      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-linear-to-r from-blue-400 to-emerald-400">
+        Vite + Tailwind v4 is Alive! ⚡
+      </h1>
+    </div>
+  )
+}
+
+export default App
